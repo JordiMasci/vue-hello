449c8c2d0b104007f2518961af809151f0b5160d
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-08-29 16:17:15 +0200

    end ex

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 // Descrizione:
 // Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
 
+// Bonus:
+// Aggiungere alla pagina un'immagine, presa anch'essa da un data.
+
 const { createApp } = Vue;
 
 createApp({
   data() {
     return {
       message: "Hello Vue!",
+      img: "img.jpg",
     };
   },
 }).mount("#app");
 
-// Bonus:
-// Aggiungere alla pagina un'immagine, presa anch'essa da un data.
